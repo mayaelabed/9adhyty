c57e8f92f1683285d90d49d02abefcfe01fcf780
khalilMaat <maatoug620@outlook.fr>
2024-04-25 17:18:39 +0100

    auth and user crud admin

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+
 
 
 const UserSchema = new mongoose.Schema(
     {
+        fullname:{
+            type: String,
+            required: "Your fullname is required"
+        },
         email: {
             type: String,
             required: "Your email is required",
@@ -17,30 +21,12 @@ const UserSchema = new mongoose.Schema(
             select: true,
             max: 25,
         },
-      role: {
-            type: String,
-            enum: ['admin', 'client'],
-        },
+        role: { type: String, default: "USER_ROLE" },
     },
     
     { timestamps: true }
 );
 
-UserSchema.pre("save", function (next) {
-    const user = this;
 
-    if (!user.isModified("password")) return next();
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) return next(err);
-
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-const User=mongoose.model("users", UserSchema)
+const User=mongoose.model("users", UserSchema);
 module.exports=User;
